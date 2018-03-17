297bbaa5299eb86a673fd3ec05dda4b88079cb67
Harsh Gandhi <hgandhi083@gmail.com>
2018-03-17 21:41:36 +0530

    commit1

---
+++ testJenkins/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testJenkins.MvcApplication" Language="C#" %>
---
+++ testJenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testJenkins.Startup))]
+namespace testJenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
